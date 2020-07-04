5c09c066d3ff05e9d0915d9a9b028b421016ab30
José Neto <josenetopr5@gmail.com>
2020-07-04 20:33:02 -0300

    Characters component done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+import axios from 'axios';
+
 import Header from './components/Header';
+import Characters from './components/Characters';
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(`https://www.breakingbadapi.com/api/characters`);
+
+      setItems(result.data);
+      setIsLoading(false);
+    }
+
+    fetchItems();
+  }, []);
+
   return (
     <div className="App">
       <Header />
+      <Characters isLoading={isLoading} items={items} />
     </div>
   );
 }
