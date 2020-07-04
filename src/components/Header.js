b1669b9bc98956c5b6544e654f5487a91afa731e
José Neto <josenetopr5@gmail.com>
2020-07-04 20:15:03 -0300

    Header done

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logoImg from '../img/logo.png'
+
+const Header = () => {
+  return (
+    <header className='center'>
+      <img src={logoImg} alt='' />
+    </header>
+  )
+};
+
+export default Header;
